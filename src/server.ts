ea597ee641ba93abbd41997ff9a31bd7f9ebc068
Pratik Majumdar <info.pratikmajumdar@gmail.com>
2022-10-07 16:17:42 +0530

    ADD: Few middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
 
 const app = express();
+
+app.use(helmet());
+app.use(morgan(process.env.NODE_ENV === "development" ? "dev" : "common"));
 
 app.listen(5000, () => {
 	console.log("Server Listening to Port 5000");
